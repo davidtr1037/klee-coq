9eeb6802c2699952dee145416de11070fc461675
David Trabish <davidtr1037@gmail.com>
2024-07-25 18:27:34 +0300

    updating

--- coq/Test.v
+++ coq/Test.v
@@ -8,6 +8,7 @@ From SE Require Import DynamicValue.
 From SE Require Import LLVMAst.
 From SE Require Import CFG.
 From SE Require Import Concrete.
+From SE Require Import IDMap.
 
 (*
 Definition x := DV_I8 (Int8.repr 1).
@@ -31,7 +32,7 @@ Definition f_block_2_id := (Name "if.then").
 Definition f_block_3_id := (Name "if.else").
 Definition f_block_4_id := (Name "return").
 
-Definition f_block_1_cmd_0 := (CMD_Inst 0 (INSTR_Op (Name "cmp") (OP_ICmp Sgt (TYPE_I 32) (EXP_Ident (ID_Local (Name "x"))) (EXP_Ident (ID_Local (Name "y")))))).
+Definition f_block_1_cmd_0 := (CMD_Inst 0 (INSTR_Op (Name "cmp") (OP_ICmp Slt (TYPE_I 32) (EXP_Ident (ID_Local (Name "x"))) (EXP_Ident (ID_Local (Name "y")))))).
 Definition f_block_1_cmd_1 := (CMD_Term 1 (TERM_Br ((TYPE_I 1), (EXP_Ident (ID_Local (Name "cmp")))) f_block_2_id f_block_3_id)).
 
 Definition f_block_1 : llvm_block := mk_block
@@ -120,9 +121,9 @@ Definition main_block_1_cmd_0 :=
   )
 .
 
-Definition main_block_1_id := (Name "entry").
-
 Definition main_block_1_cmd_1 := (CMD_Term 1 (TERM_Ret ((TYPE_I 32), (EXP_Integer (0)%Z)))).
+
+Definition main_block_1_id := (Name "entry").
 
 Definition main_block_1 : llvm_block := mk_block
   main_block_1_id
@@ -133,10 +134,8 @@ Definition main_block_1 : llvm_block := mk_block
   None
 .
 
-Definition main_entry := (Name "entry").
-
 Definition main_cfg : llvm_cfg := mkCFG
-  main_entry
+  main_block_1_id
   [main_block_1]
   []
 .
@@ -161,7 +160,7 @@ Definition m : llvm_module := mk_module
 .
 
 Definition s_0 := mk_state
-  (mk_inst_counter main_id main_entry 0)
+  (mk_inst_counter main_id main_block_1_id 0)
   main_block_1_cmd_0
   [main_block_1_cmd_1]
   None
@@ -170,6 +169,102 @@ Definition s_0 := mk_state
   empty_dv_store
   m
 .
+
+Definition s_1 := mk_state
+  (mk_inst_counter f_id f_entry 0)
+  f_block_1_cmd_0
+  [f_block_1_cmd_1]
+  None
+  ((Name "x") !-> (DV_I32 (repr 1)); (Name "y") !-> (DV_I32 (repr 2)); empty_dv_store)
+  [Frame empty_dv_store (mk_inst_counter main_id main_block_1_id 1) None (Name "call")]
+  empty_dv_store
+  m
+.
+
+Lemma L_1 : step s_0 s_1.
+Proof.
+  apply (Step_Call
+    (mk_inst_counter main_id main_block_1_id 0)
+    0
+    (Name "call")
+    (TYPE_I 32)
+    (EXP_Ident (ID_Global f_id))
+    [(((TYPE_I 32), (EXP_Integer (1)%Z)), []); (((TYPE_I 32), (EXP_Integer (2)%Z)), [])]
+    _
+    main_block_1_cmd_1
+    []
+    None
+    empty_dv_store
+    []
+    empty_dv_store
+    m
+    f_def
+    f_block_1
+    f_block_1_cmd_0
+    [f_block_1_cmd_1]
+    [(DV_I32 (repr 1)); (DV_I32 (repr 2))]
+    ((Name "x") !-> (DV_I32 (repr 1)); (Name "y") !-> (DV_I32 (repr 2)); empty_dv_store)
+  ).
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+Qed.
+
+Definition s_2 := mk_state
+  (mk_inst_counter f_id f_entry 1)
+  f_block_1_cmd_1
+  []
+  None
+  ((Name "cmp") !-> (DV_I1 one); (Name "x") !-> (DV_I32 (repr 1)); (Name "y") !-> (DV_I32 (repr 2)); empty_dv_store)
+  [Frame empty_dv_store (mk_inst_counter main_id main_block_1_id 1) None (Name "call")]
+  empty_dv_store
+  m
+.
+
+Lemma L_2 : step s_1 s_2.
+Proof.
+  apply Step_OP.
+  reflexivity.
+Qed.
+
+Definition s_3 := mk_state
+  (mk_inst_counter f_id f_block_2_id 0)
+  f_block_2_cmd_0
+  []
+  (Some f_block_1_id)
+  ((Name "cmp") !-> (DV_I1 one); (Name "x") !-> (DV_I32 (repr 1)); (Name "y") !-> (DV_I32 (repr 2)); empty_dv_store)
+  [Frame empty_dv_store (mk_inst_counter main_id main_block_1_id 1) None (Name "call")]
+  empty_dv_store
+  m
+.
+
+Lemma L_3 : step s_2 s_3.
+Proof.
+  apply (Step_Br_True
+    (mk_inst_counter f_id f_entry 1)
+    1
+    (TYPE_I 1)
+    (EXP_Ident (ID_Local (Name "cmp")))
+    f_block_2_id
+    f_block_3_id
+    None
+    ((Name "cmp") !-> (DV_I1 one); (Name "x") !-> (DV_I32 (repr 1)); (Name "y") !-> (DV_I32 (repr 2)); empty_dv_store)
+    [Frame empty_dv_store (mk_inst_counter main_id main_block_1_id 1) None (Name "call")]
+    empty_dv_store
+    m
+    f_def
+    f_block_2
+    f_block_2_cmd_0
+    []
+  ).
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+  - reflexivity.
+Qed.
 
 Definition final_state := mk_state
   (mk_inst_counter main_id main_entry 1)
