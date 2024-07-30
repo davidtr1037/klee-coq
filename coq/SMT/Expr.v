0c846633cf0e5d033adaf6987b0bd17a1c0da62f
David Trabish <davidtr1037@gmail.com>
2024-07-30 16:57:27 +0300

    updating

--- coq/SMT/Expr.v
+++ coq/SMT/Expr.v
@@ -2,14 +2,8 @@ From Coq Require Import Strings.String.
 From Coq Require Import ZArith.
 
 From SE Require Import BitVectors.
+From SE Require Import DynamicValue.
 From SE.Numeric Require Import Integers.
-
-(*
-Definition array_model 
-From SE Require Import Maps.
-Definition smt_model := total_map nat.
-Definition default_model := empty_map 0
-*)
 
 Inductive smt_expr : Type :=
   | SMT_Const_I1 (n : int1)
@@ -45,17 +39,20 @@ Inductive smt_expr : Type :=
   | SMT_Sle (e1 e2 : smt_expr)
   | SMT_Sgt (e1 e2 : smt_expr)
   | SMT_Sge (e1 e2 : smt_expr)
-  | SMT_ZExt (e : smt_expr)
-  | SMT_SExt (e : smt_expr)
+  | SMT_ZExt (e : smt_expr) (w : nat)
+  | SMT_SExt (e : smt_expr) (w : nat)
   | SMT_ITE (c e1 e2 : smt_expr)
   | SMT_Not (e : smt_expr)
   | SMT_Concat (e1 e2 : smt_expr)
-  | SMT_Extract (e : smt_expr) (o w : nat)
+  | SMT_Extract (e : smt_expr) (i w : nat)
   | SMT_Select (a : smt_array) (e : smt_expr)
   with smt_array : Type :=
     | SMT_Array (x : string)
     | SMT_Store (a : smt_array) (i e : smt_expr)
 .
+
+Definition SMT_True := SMT_Const_I1 one.
+Definition SMT_False := SMT_Const_I1 zero.
 
 Inductive smt_sort : Type :=
   | Sort_BV1
@@ -65,37 +62,3 @@ Inductive smt_sort : Type :=
   | Sort_BV64
   | Sort_Array
 .
-
-Module Symbol.
-Inductive symbol : Type :=
-  | Symbol_BV (s : string)
-  | Symbol_Array (s : string)
-.
-
-(* TODO: use a module type / class *)
-Definition symbol_eqb (x y : symbol) : bool :=
-  match x, y with
-  | Symbol_BV s1, Symbol_BV s2 => String.eqb s1 s2
-  | Symbol_Array s1, Symbol_Array s2 => String.eqb s1 s2
-  | _, _ => false
-  end
-.
-
-Lemma symbol_eqb_refl :
-  forall x, symbol_eqb x x = true.
-Proof.
-Admitted.
-
-Lemma symbol_eqb_eq :
-  forall x y, symbol_eqb x y = true <-> x = y.
-Proof.
-Admitted.
-
-Lemma symbol_eqb_neq :
-  forall x y, symbol_eqb x y = false <-> x <> y.
-Proof.
-Admitted.
-
-Module End.
-
-Definition array_model := Type.
