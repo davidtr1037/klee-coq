cf1a694d37769cbe4cf4f8219f4c7642c7337e88
David Trabish <davidtr1037@gmail.com>
2024-08-08 16:28:08 +0300

    adding files

---
+++ coq/Completeness.v
@@ -0,0 +1,27 @@
+From SE Require Import BitVectors.
+From SE Require Import CFG.
+From SE Require Import Concrete.
+From SE Require Import DynamicValue.
+From SE Require Import IDMap.
+From SE Require Import LLVMAst.
+From SE Require Import Symbolic.
+From SE Require Import Relation.
+From SE Require Import WellDefined.
+
+From SE.SMT Require Import Expr.
+From SE.SMT Require Import Model.
+
+Lemma completeness_single_step :
+  forall c c' s,
+    step c c' -> well_defined s -> over_approx s c -> (exists s', sym_step s s' /\ over_approx s' c').
+Proof.
+Admitted.
+
+Lemma completeness :
+  forall (mdl : llvm_module) (d : llvm_definition) (c : state),
+    exists init_c,
+      (init_state mdl d) = Some init_c ->
+      multi_step init_c c ->
+      (exists init_s s, (init_sym_state mdl d) = Some init_s /\ multi_sym_step init_s s /\ over_approx s c).
+Proof.
+Admitted.
---
+++ coq/Soundness.v
@@ -0,0 +1,30 @@
+From SE Require Import BitVectors.
+From SE Require Import CFG.
+From SE Require Import Concrete.
+From SE Require Import DynamicValue.
+From SE Require Import IDMap.
+From SE Require Import LLVMAst.
+From SE Require Import Symbolic.
+From SE Require Import Relation.
+
+From SE.SMT Require Import Expr.
+From SE.SMT Require Import Model.
+
+Lemma soundness_single_step :
+  forall s s' c m,
+    sym_step s s' ->
+    over_approx_via s c m ->
+    sat_sym_state m s' ->
+    (exists c', step c c' /\ over_approx_via s' c' m).
+Proof.
+Admitted.
+
+Lemma soundness :
+  forall (mdl : llvm_module) (d : llvm_definition) (s : sym_state) (m : smt_model),
+    exists init_s,
+      (init_sym_state mdl d) = Some init_s ->
+      multi_sym_step init_s s ->
+      sat_sym_state m s ->
+      (exists init_c c, (init_state mdl d) = Some init_c /\ (multi_step c c) /\ over_approx_via s c m).
+Proof.
+Admitted.
