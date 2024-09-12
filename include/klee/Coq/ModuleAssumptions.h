615a22a8f21171f3dbafb7145a5daaf7ea8f5c27
David Trabish <davidtr1037@gmail.com>
2024-09-12 11:17:30 +0300

    adding files

---
+++ include/klee/Coq/ModuleAssumptions.h
@@ -0,0 +1,44 @@
+#ifndef KLEE_MODULEASSUMPTIONS_H
+#define KLEE_MODULEASSUMPTIONS_H
+
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+
+#include "klee/Coq/CoqLanguage.h"
+#include "klee/Coq/Translation.h"
+
+#include <map>
+
+namespace klee {
+
+class ModuleSupport {
+
+public:
+
+  llvm::Module &m;
+
+  ModuleTranslator &moduleTranslator;
+
+  ModuleSupport(llvm::Module &m, ModuleTranslator &moduleTranslator);
+
+  ref<CoqTactic> generateProof();
+
+  ref<CoqExpr> getLemmaForFunction(llvm::Function &f);
+
+  ref<CoqTactic> getTacticForFunction(llvm::Function &f);
+
+  ref<CoqExpr> getLemmaForBasicBlock(llvm::BasicBlock &bb);
+
+  ref<CoqTactic> getTacticForBasicBlock(llvm::BasicBlock &bb);
+
+  ref<CoqExpr> getLemmaForInst(llvm::Instruction &inst);
+
+  ref<CoqTactic> getTacticForInst(llvm::Instruction &inst);
+
+  ~ModuleSupport();
+};
+
+}
+
+#endif
---
+++ lib/Coq/ModuleAssumptions.cpp
@@ -0,0 +1,90 @@
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/Operator.h"
+#include "llvm/IR/Constants.h"
+
+#include "klee/Coq/CoqLanguage.h"
+#include "klee/Coq/Translation.h"
+#include "klee/Coq/ModuleAssumptions.h"
+
+#include <map>
+#include <string>
+
+using namespace llvm;
+using namespace klee;
+
+ModuleSupport::ModuleSupport(Module &m, ModuleTranslator &moduleTranslator) :
+  m(m), moduleTranslator(moduleTranslator) {
+
+}
+
+ref<CoqTactic> ModuleSupport::generateProof() {
+  return nullptr;
+}
+
+ref<CoqExpr> ModuleSupport::getLemmaForFunction(Function &f) {
+  /* TODO: use aliases */
+  ref<CoqExpr> body = new CoqApplication(
+    new CoqVariable("is_supported_definition"),
+    {moduleTranslator.translateFunction(f)}
+  );
+
+  ref<CoqTactic> proof = getTacticForFunction(f);
+
+  return new CoqLemma(
+    "is_supported_?",
+    body,
+    proof,
+    true
+  );
+}
+
+ref<CoqTactic> ModuleSupport::getTacticForFunction(Function &f) {
+  return new Block({new Admit()});
+}
+
+ref<CoqExpr> ModuleSupport::getLemmaForBasicBlock(BasicBlock &bb) {
+  ref<CoqExpr> body = new CoqApplication(
+    new CoqVariable("is_supported_block"),
+    {moduleTranslator.translateBasicBlock(bb)}
+  );
+
+  ref<CoqTactic> proof = getTacticForBasicBlock(bb);
+
+  return new CoqLemma(
+    "is_supported_?",
+    body,
+    proof,
+    true
+  );
+}
+
+ref<CoqTactic> ModuleSupport::getTacticForBasicBlock(BasicBlock &bb) {
+  return new Block({new Admit()});
+}
+
+ref<CoqExpr> ModuleSupport::getLemmaForInst(Instruction &inst) {
+  /* TODO: use aliases */
+  ref<CoqExpr> body = new CoqApplication(
+    new CoqVariable("is_supported_cmd"),
+    {moduleTranslator.translateInst(inst)}
+  );
+
+  ref<CoqTactic> proof = getTacticForInst(inst);
+
+  return new CoqLemma(
+    "is_supported_?",
+    body,
+    proof,
+    true
+  );
+}
+
+ref<CoqTactic> ModuleSupport::getTacticForInst(Instruction &inst) {
+  return new Block({new Admit()});
+}
+
+ModuleSupport::~ModuleSupport() {
+
+}
