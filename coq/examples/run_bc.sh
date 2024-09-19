ff955de518294bf183df07c429151553f33f2a23
David Trabish <davidtr1037@gmail.com>
2024-09-19 17:38:22 +0300

    adding file

---
+++ coq/examples/run_bc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+CURRENT_PATH=$(dirname ${BASH_SOURCE[0]})
+ROOT=$(realpath ${CURRENT_PATH}/../..)
+KLEE=klee
+
+function run_klee {
+    output=${ROOT}/coq/out.v
+    debug_output=${ROOT}/coq/debug.v
+    po_output=/tmp/po
+    bc_file=$1
+    echo "testing ${bc_file}"
+    $KLEE \
+        -search=dfs \
+        -generate-proof \
+        -proof-output-path=${output} \
+        -proof-debug-script-path=${debug_output} \
+        -decompose-state \
+        -optimize-proof \
+        $1 &> /dev/null
+    time coqc -Q ${ROOT}/coq SE ${output}
+    coqc -Q ${ROOT}/coq SE ${debug_output} > ${po_output}
+    ls -lh ${po_output}
+}
+
+run_klee $1
