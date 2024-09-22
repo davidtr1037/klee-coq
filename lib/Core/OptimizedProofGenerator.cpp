d818674d1a3d8cf13c708bf1ce4ab120087f99fd
David Trabish <davidtr1037@gmail.com>
2024-09-22 11:39:03 +0300

    using equiv_sym_state_br_unsat_sat

--- lib/Core/OptimizedProofGenerator.cpp
+++ lib/Core/OptimizedProofGenerator.cpp
@@ -664,6 +664,21 @@ klee::ref<CoqLemma> OptimizedProofGenerator::createLemmaForSubtree(StateInfo &st
   BasicBlock *bb = bi->getParent();
   Function *f = bb->getParent();
 
+  ref<CoqExpr> cond = new CoqApplication(
+    new CoqVariable("extract_ast"),
+    {
+      new CoqApplication(
+        new CoqVariable("sym_eval_exp"),
+        {
+          getLocalStoreAlias(stateInfo.stepID),
+          new CoqVariable("gs"), /* TODO: replace */
+          createSome(moduleTranslator->createTypeI(1)),
+          moduleTranslator->translateBranchInstExpr(bi),
+        }
+      ),
+    }
+  );
+
   if (si1.isSat && !si2.isSat) {
     ref<CoqExpr> satPC = exprTranslator->translate(si1.satPC,
                                                    &si1.state->arrayTranslation);
@@ -675,21 +690,6 @@ klee::ref<CoqLemma> OptimizedProofGenerator::createLemmaForSubtree(StateInfo &st
 
     Instruction *targetInst = si1.state->pc->inst;
     BasicBlock *targetBB = targetInst->getParent();
-
-    ref<CoqExpr> cond = new CoqApplication(
-      new CoqVariable("extract_ast"),
-      {
-        new CoqApplication(
-          new CoqVariable("sym_eval_exp"),
-          {
-            getLocalStoreAlias(stateInfo.stepID),
-            new CoqVariable("gs"), /* TODO: replace */
-            createSome(moduleTranslator->createTypeI(1)),
-            moduleTranslator->translateBranchInstExpr(bi),
-          }
-        ),
-      }
-    );
 
     ref<CoqTactic> t = new Block(
       {
@@ -724,6 +724,60 @@ klee::ref<CoqLemma> OptimizedProofGenerator::createLemmaForSubtree(StateInfo &st
         new Block({new Reflexivity()}),
         new Block({new Reflexivity()}),
         new Block({new Apply("L_" + to_string(si1.state->stepID))}),
+        new Block({new Apply("equiv_smt_expr_normalize_simplify")}),
+        new Block({new Apply("equiv_smt_expr_normalize_simplify")}),
+        new Block({new Apply("UNSAT_" + to_string(axiomID))}),
+      }
+    );
+
+    return createLemma(stateInfo.stepID, t);
+  }
+
+  if (!si1.isSat && si2.isSat) {
+    ref<CoqExpr> satPC = exprTranslator->translate(si2.satPC,
+                                                   &si2.state->arrayTranslation);
+    ref<CoqExpr> unsatPC = exprTranslator->translate(si1.unsatPC,
+                                                     &si2.state->arrayTranslation);
+    uint64_t axiomID = allocateAxiomID();
+    ref<CoqLemma> lemma = getUnsatAxiom(unsatPC, axiomID);
+    unsatAxioms.push_front(lemma);
+
+    Instruction *targetInst = si2.state->pc->inst;
+    BasicBlock *targetBB = targetInst->getParent();
+
+    ref<CoqTactic> t = new Block(
+      {
+        new Apply(
+          "equiv_sym_state_br_unsat_sat",
+          {
+            getICAlias(stateInfo.stepID),
+            createNat(moduleTranslator->getInstID(bi)),
+            moduleTranslator->translateBranchInstExpr(bi),
+            moduleTranslator->translateBranchInstBid(bi, 0),
+            moduleTranslator->translateBranchInstBid(bi, 1),
+            getPrevBIDAlias(stateInfo.stepID),
+            getLocalStoreAlias(stateInfo.stepID),
+            getStackAlias(stateInfo.stepID),
+            createPlaceHolder(), /* TODO: pass argument */
+            getSymbolicsAlias(stateInfo.stepID),
+            getPCAlias(stateInfo.stepID),
+            createModule(),
+            cond,
+            moduleTranslator->translateFunctionCached(*f),
+            moduleTranslator->translateBasicBlockCached(*targetBB),
+            getCommandAlias(si2.state->stepID),
+            getCommandsAlias(si2.state->stepID),
+            satPC,
+            unsatPC,
+            getTreeAlias(si2.state->stepID),
+          }
+        ),
+        new Block({new Reflexivity()}),
+        new Block({new Reflexivity()}),
+        new Block({new Reflexivity()}),
+        new Block({new Reflexivity()}),
+        new Block({new Reflexivity()}),
+        new Block({new Apply("L_" + to_string(si2.state->stepID))}),
         new Block({new Apply("equiv_smt_expr_normalize_simplify")}),
         new Block({new Apply("equiv_smt_expr_normalize_simplify")}),
         new Block({new Apply("UNSAT_" + to_string(axiomID))}),
