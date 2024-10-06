bacd8e709375e355f887a16c341f5457f4368202
David Trabish <davidtr1037@gmail.com>
2024-10-06 10:45:08 +0300

    updating test

--- coq/examples/test_12.c
+++ coq/examples/test_12.c
@@ -4,27 +4,24 @@
 
 #include <klee/klee.h>
 
-/* TODO: test */
-void f1() {
+void test_urem_1() {
     unsigned x = 3;
     unsigned y = 2;
-    unsigned n = klee_make_symbolic_int32();
-    unsigned b = n % x;
-}
-
-/* TODO: test */
-void f2(unsigned x, unsigned y) {
     unsigned a = x % y;
+    unsigned n = klee_make_symbolic_int32();
+    unsigned b = n % a;
 }
 
-void f3() {
-    unsigned n = klee_make_symbolic_int32();
-    unsigned a = n % 1;
+void test_srem_1() {
+    int x = 3;
+    int y = 2;
+    int a = x % y;
+    int n = klee_make_symbolic_int32();
+    int b = n % a;
 }
 
 int main() {
-    f1();
-    //f2(2, 3);
-    //f3();
+    test_urem_1();
+    test_srem_1();
     return 0;
 }
