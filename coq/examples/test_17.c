b2d3496da37e42a17178149c984f8ae11bf23af3
David Trabish <davidtr1037@gmail.com>
2024-10-09 17:35:57 +0300

    adding file

---
+++ coq/examples/test_17.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <assert.h>
+
+#include <klee/klee.h>
+
+void f1(unsigned x) {
+    unsigned y = x ^ 2;
+}
+
+void f2() {
+    unsigned x = klee_make_symbolic_int32();
+    unsigned y = x ^ 7;
+}
+
+void f3() {
+    unsigned x = klee_make_symbolic_int32();
+    unsigned y = 7 ^ x;
+}
+
+int main() {
+    f1(100);
+    f2();
+    f3();
+    return 0;
+}
+
