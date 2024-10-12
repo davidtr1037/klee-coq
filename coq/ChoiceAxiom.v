23ad1b8d21a45961ed2fa961a0fbb463488a1e21
David Trabish <davidtr1037@gmail.com>
2024-10-12 23:57:24 +0300

    updating

--- coq/ChoiceAxiom.v
+++ coq/ChoiceAxiom.v
@@ -11,6 +11,4 @@ Admitted.
 Definition extend_names (l : list string) : list string :=
   (fresh_name l) :: l.
 
-Lemma fresh_name_lemma : forall l, ~ In (fresh_name l) l.
-Proof.
-Admitted.
+Axiom fresh_name_lemma : forall l, ~ In (fresh_name l) l.
