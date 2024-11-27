e1f091122baa4c82ce9905c0ba1d2e87dc5aa293
David Trabish <davidtr1037@gmail.com>
2024-11-27 11:04:37 +0200

    updating

--- coq/Relation.v
+++ coq/Relation.v
@@ -4,3 +4,21 @@ Inductive multi {X : Type} (R : relation X) : relation X :=
   | multi_base : forall (x y : X), R x y -> multi R x y
   | multi_trans : forall (x y z : X), multi R x y -> R y z -> multi R x z
 .
+
+Lemma relation_concat {X : Type} : forall (R : relation X) (x y z : X),
+  (multi R) x y -> (multi R) y z -> (multi R) x z.
+Proof.
+  intros R x y z H1 H2.
+  induction H2 as [y z | y y' z].
+  {
+    eapply multi_trans.
+    { eassumption. }
+    { assumption. }
+  }
+  {
+    apply IHmulti in H1.
+    eapply multi_trans.
+    { eassumption. }
+    { assumption. }
+  }
+Qed.
