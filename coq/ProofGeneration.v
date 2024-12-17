1e252e55697ccf7af0cb541787497dec071f7d33
David Trabish <davidtr1037@gmail.com>
2024-12-17 17:30:16 +0200

    removing unsat_ne_i32

--- coq/ProofGeneration.v
+++ coq/ProofGeneration.v
@@ -391,39 +391,6 @@ Proof.
   assumption.
 Qed.
 
-Lemma unsat_ne_i32 : forall n1 n2,
-  eq n1 n2 = false ->
-  unsat (AST_CmpOp Sort_BV32 SMT_Eq (AST_Const Sort_BV32 n1) (AST_Const Sort_BV32 n2)).
-Proof.
-  intros n1 n2 Hne.
-  unfold unsat.
-  intros Hsat.
-  unfold sat in Hsat.
-  destruct Hsat as [m Hsat].
-  unfold sat_via in Hsat.
-  simpl in Hsat.
-  unfold smt_eval_cmpop_by_sort in Hsat.
-  simpl in Hsat.
-  simpl in Hne.
-  rewrite Hne in Hsat.
-  discriminate.
-Qed.
-
-Lemma unsat_extension_with_ne_i32 : forall pc n1 n2,
-  eq n1 n2 = false ->
-  unsat
-    (AST_BinOp
-      Sort_BV1
-      SMT_And
-      pc
-      (AST_CmpOp Sort_BV32 SMT_Eq (AST_Const Sort_BV32 n1) (AST_Const Sort_BV32 n2))).
-Proof.
-  intros pc n1 n2 Heq.
-  apply unsat_and_right.
-  apply unsat_ne_i32.
-  assumption.
-Qed.
-
 (* TODO: rename? *)
 Lemma equiv_smt_expr_eq_zero_zext_bv32_bv64 : forall ast,
   equiv_smt_expr
