6d69dabe5c7aa20bbecb910fc5a4925545edf789
David Trabish <davidtr1037@gmail.com>
2024-12-22 14:00:20 +0200

    updating test

--- coq/examples/test_12.c
+++ coq/examples/test_12.c
@@ -12,6 +12,16 @@ void test_urem_1() {
     unsigned b = n % a;
 }
 
+void test_urem_2(unsigned x) {
+    unsigned y = 3 % x;
+}
+
+void test_urem_3() {
+    unsigned x = klee_make_symbolic_int32();
+    klee_assume_bool(x != 0);
+    unsigned y = 3 % x;
+}
+
 void test_srem_1() {
     int x = 3;
     int y = 2;
@@ -20,8 +30,22 @@ void test_srem_1() {
     int b = n % a;
 }
 
+void test_srem_2(int x) {
+    int y = 3 % x;
+}
+
+void test_srem_3() {
+    int x = klee_make_symbolic_int32();
+    klee_assume_bool(x != 0);
+    int y = 3 % x;
+}
+
 int main() {
     test_urem_1();
+    test_urem_2(10);
+    test_urem_3();
     test_srem_1();
+    test_srem_2(10);
+    test_srem_3();
     return 0;
 }
