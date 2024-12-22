17a82d0b91f6939e65ce618d520d756dec295014
David Trabish <davidtr1037@gmail.com>
2024-12-22 18:00:54 +0200

    updating tests

--- coq/examples/test_16.c
+++ coq/examples/test_16.c
@@ -14,6 +14,17 @@ void test_ashr_2() {
     int y = x >> 7;
 }
 
+void test_ashr_3(int x) {
+    int y = 1 >> x;
+}
+
+void test_ashr_4() {
+    int x = klee_make_symbolic_int32();
+    klee_assume_bool(x < 32);
+    klee_assume_bool(x >= 0);
+    int y = 1 >> x;
+}
+
 void test_lshr_1(unsigned x) {
     unsigned y = x >> 2;
 }
@@ -23,10 +34,25 @@ void test_lshr_2() {
     unsigned y = x >> 7;
 }
 
+void test_lshr_3(unsigned x) {
+    unsigned y = 1 >> x;
+}
+
+void test_lshr_4() {
+    unsigned x = klee_make_symbolic_int32();
+    klee_assume_bool(x < 32);
+    klee_assume_bool(x >= 0);
+    unsigned y = 1 >> x;
+}
+
 int main() {
     test_ashr_1(100);
     test_ashr_2();
+    test_ashr_3(10);
+    test_ashr_4();
     test_lshr_1(100);
     test_lshr_2();
+    test_lshr_3(10);
+    test_lshr_4();
     return 0;
 }
