6bb0420a714f171b4cc901d651ee1dcb19ad1804
David Trabish <davidtr1037@gmail.com>
2025-01-20 13:14:37 +0200

    updating

--- coq/examples/run_bc.sh
+++ coq/examples/run_bc.sh
@@ -3,6 +3,20 @@ CURRENT_PATH=$(dirname ${BASH_SOURCE[0]})
 ROOT=$(realpath ${CURRENT_PATH}/../..)
 KLEE=klee
 
+function run_klee_no_opt {
+    output=${ROOT}/coq/out.v
+    bc_file=$1
+    echo "testing ${bc_file}"
+    $KLEE \
+        -search=dfs \
+        -rewrite-equalities=0 \
+        -generate-proof \
+        -proof-output-path=${output} \
+        $1
+    time coqc -Q ${ROOT}/coq SE ${output}
+    ls -lh ${output}
+}
+
 function run_klee {
     output=${ROOT}/coq/out.v
     bc_file=$1
